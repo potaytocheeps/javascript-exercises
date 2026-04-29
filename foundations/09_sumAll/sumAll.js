1c1bd0f0c2921cd75e59a47d0a3b2cc630b25d89
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2026-04-29 13:25:41 -0700

    Add solution for exericse: 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return "ERROR";
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
 
+    let sum = 0;
+
+    if (start > end)
+    {
+        [start, end] = [end, start];
+    }
+
+    for (let current = start; current <= end; current++)
+    {
+        sum += current;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
