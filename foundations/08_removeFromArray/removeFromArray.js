74ca9d3fda4e34261369e1d721a7ef347479ca25
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2026-04-29 12:32:19 -0700

    Add solution for exercise: 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    let newArray = array.slice();
+
+    for (const valueToRemove of valuesToRemove)
+    {
+        newArray = newArray.filter(value => value !== valueToRemove);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
