de9328c7c38fe5218ed4f72dc6e3f66a1d6cde92
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2026-04-29 13:48:06 -0700

    Add solution for exercise: 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+    let celsius = (fahrenheitTemp - 32) / 1.8;
+
+    return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+    let fahrenheit = (celsiusTemp * 1.8) + 32;
+
+    return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
