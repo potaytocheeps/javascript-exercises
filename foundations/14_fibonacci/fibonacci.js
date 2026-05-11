4980037bf858782129f23e87bcf9f936c6c782dd
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2026-05-11 00:47:10 -0700

    Add solution for exercise: 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (isNaN(parseInt(index))) return 0;
 
+    index = Number(index);
+
+    if (index < 0) return "OOPS";
+    if (index === 0) return 0;
+
+    const sequence = [1, 1];
+
+    for (let i = 2; i < index; i++)
+    {
+        sequence.push(sequence[i - 2] + sequence[i - 1]);
+    }
+
+    return sequence[index - 1];
 };
 
 // Do not edit below this line
