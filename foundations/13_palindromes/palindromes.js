4323735c3beb0a980f7ae431e31bf9914d3c427f
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2026-05-11 00:30:13 -0700

    Add solution for exercise: 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (text = "") {
+    text = text.toLowerCase();
 
+    const textArray = text.split('');
+
+    let filteredTextArray = textArray.filter(letter => (letter >= "a" && letter <= "z") || Number.isInteger(+letter));
+    filteredTextArray = filteredTextArray.filter(letter => letter !== ' ');
+
+    const filteredText = filteredTextArray.join('');
+    const filteredTextReversed = [...filteredTextArray].reverse().join('');
+
+    return filteredText === filteredTextReversed;
 };
 
 // Do not edit below this line
