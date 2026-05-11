51e9f9ea56544e5f916803f8896b13cb789f8a05
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2026-05-11 01:18:18 -0700

    Add solution for exercise: 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function getAge(person)
+{
+    if ("yearOfDeath" in person)
+    {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    else
+    {
+        return (new Date()).getFullYear() - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        let currentPersonAge = getAge(currentPerson);
+        let oldestAge = getAge(oldestPerson);
+
+        return (currentPersonAge > oldestAge) ? currentPerson : oldestPerson;
+    });
 };
 
 // Do not edit below this line
